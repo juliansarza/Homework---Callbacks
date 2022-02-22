1b2541a5ad0c440c723ca3a4dabfaf4605814df7
Julian Sarza <julian2570@gmail.com>
2022-02-21 17:21:19 -0800

    Add asynchronous callbacks

---
+++ app.js
@@ -0,0 +1,30 @@
+setTimeout(six, 6000);
+setTimeout(five, 5000);
+setTimeout(four, 4000);
+setTimeout(three, 3000);
+setTimeout(two, 2000);
+setTimeout(one, 1000);
+ 
+  function six() {
+    console.log("Six");
+  }
+
+  function five() {
+    console.log("Five");
+  }
+
+  function four() {
+    console.log("Four");
+  }
+
+  function three() {
+      console.log("Three");
+  }
+
+  function two() {
+    console.log("Two");
+  }
+
+  function one() {
+      console.log("One")
+  }
